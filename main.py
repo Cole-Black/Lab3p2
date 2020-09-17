1d28628bd1a12f575f9949defe7f49aa28c8b2dd
ColeBlack3 <blackstallardc@gmail.com>
2020-09-17 21:59:10 +0000

    str

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 
-
-
-
 def sum_n(n):
   if n == 0:
     return n 
   return n + sum_n(n-1)
 
-def print_n(s, n):
+def print_n(str, n):
   if n == 0:
     return
-  print(f"{s}")
-  print_n(s, n - 1)
+  print(f"{str}")
+  print_n(str, n - 1)
 
 def run():
   nIN = int(input("Enter an int: "))
   print(f"sum is {sum_n(nIN)}.")
 
-  sIN = input("Enter a string: ")
-  print_n(sIN, nIN)
-
+  str = input("Enter a string: ")
+  print_n(str, nIN)
 
 if __name__ == "__main__":
   run()
