c25aa22daceed51ced2eedf2dd1ff18e87dd79d4
ColeBlack3 <blackstallardc@gmail.com>
2020-09-17 21:26:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+
+
+def sum_n(n):
+  if n == 0:
+    return n 
+  return n + sum_n(n-1)
+
+def print_n(s, n):
+  if n == 0:
+    return
+  print(f"{s}")
+  print_n(s, n - 1)
+
+def run():
+  nIN = int(input("Enter int: "))
+  print(f"sum is: {sum_n(nIN)}.")
+
+  sIN = input("Enter a string: ")
+  print_n(sIN, nIN)
+
+
+if __name__ == "__main__":
+  run()
