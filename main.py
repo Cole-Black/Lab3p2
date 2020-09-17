200fcb79d56daadd428a02398250c44afd4dc7d4
ColeBlack3 <blackstallardc@gmail.com>
2020-09-17 22:04:22 +0000

    comments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#Author: Cole Black-Stallard cdb5655@psu.edu
+#Collaborator: Nicholas Cole nyc5096@psu.edu
+#Collaborator: Khalil Stroman kzs5955@psu.edu
+#Collaborator: Emanuel Bassill Chuckran eab6017@psu.edu
+#Section: 012R
+#Breakout: 8 
 
 def sum_n(n):
   if n == 0:
@@ -13,7 +19,6 @@ def print_n(str, n):
 def run():
   nIN = int(input("Enter an int: "))
   print(f"sum is {sum_n(nIN)}.")
-
   str = input("Enter a string: ")
   print_n(str, nIN)
 
